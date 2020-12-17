ee3b60d2243489620eef86845bc25858f67f4deb
ZakaryaNoori <ZakaryaNoori979@gmail.com>
2020-12-17 18:32:58 +0430

    Add main page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,22 @@
 <template>
   <Layout>
-
+    <div class="main-content">
+      <figure class="profile">
+        <img class="profile-img" src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/131049787_2871588396386232_8679906833486150559_o.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=QpcN2_Quk7wAX-f4Sge&_nc_ht=scontent-frt3-1.xx&oh=4e5972cc10cac9f86684e0fd6ec632b4&oe=5FFF5865" alt="me">
+      </figure>
+      <div class="intro-text">Hi, I am Zakarya. 🤞</div>
+      <div class="introduction">
+        I am a Software <span class="styled">Engineer.</span> <br>
+        I build the world <br> where we live in today.
+      </div>
+    </div>
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Home'
+<static-query>
+query {
+  metadata {
+    siteName
   }
 }
-</script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+</static-query>
